3558a274d79f74ad602f5fa177c25499d739d553
isvictorious <me@isvictorious.com>
2020-01-03 10:29:00 -0500

    completed part 8 of 100 days of Gatbsy. Day 3

--- tutorial-part-four/src/templates/blog-post.js
+++ tutorial-part-four/src/templates/blog-post.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
@@ -15,12 +17,13 @@ export default ({ data }) => {
 }
 
 export const query = graphql`
-  query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      html
-      frontmatter {
-        title
-      }
-    }
-  }
-`
+         query($slug: String!) {
+           markdownRemark(fields: { slug: { eq: $slug } }) {
+             html
+             frontmatter {
+               title
+             }
+             excerpt
+           }
+         }
+       `
